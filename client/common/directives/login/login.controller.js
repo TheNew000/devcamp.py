dca6425a8a2605083902bc4df02ce3e3b6f9d87c
Jackson Yeager <jacksonyeager@gmail.com>
2016-10-13 13:57:18 -0400

    Added login modal and arrive.js

---
+++ client/common/directives/login/login.controller.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Jackson on 10/13/16.
+ */
+
+(function () {
+    function loginController($scope){
+
+    }
+
+    angular.module('devcamp')
+        .controller('loginCtrl', ['$scope', loginController]);
+})();
